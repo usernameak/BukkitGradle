5c256da4dbce86fc9ae215a41c3ce149177da73c
usernameak <sitemakers.3dn.ru@gmail.com>
2020-11-26 21:39:13 +0200

    Update Constants.groovy

--- src/main/groovy/ru/endlesscode/bukkitgradle/server/Constants.groovy
+++ src/main/groovy/ru/endlesscode/bukkitgradle/server/Constants.groovy
@@ -13,6 +13,6 @@ class PaperConstants {
     public static final String FILE_PAPERCLIP = "paperclip.jar"
     public static final String FILE_PAPER_VERSIONS = "paper-versions.json"
 
-    public static final String URL_PAPER_VERSIONS = "https://gist.githubusercontent.com/osipxd/6119732e30059241c2192c4a8d2218d9/raw/7d2b9f6eaa982edebf1147ece8439dacd5f33d16/$FILE_PAPER_VERSIONS"
+    public static final String URL_PAPER_VERSIONS = "https://gist.githubusercontent.com/usernameak/ff46b3d4e407b0e86a8a61785555966d/raw/$FILE_PAPER_VERSIONS"
     public static final String URL_PAPER_DEFAULT = "https://papermc.io/ci/job/Paper-1.15/lastSuccessfulBuild/artifact/paperclip.jar"
 }
